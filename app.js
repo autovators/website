081394d0deeb727f0bfdc71b31521eeedbd1e0d8
Autovators <autovatorswithana@gmail.com>
2022-04-18 18:13:39 +0400

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
          header.style.backgroundColor ="rgb(0,0,0)";
      }
      else{
-         header.style.backgroundColor="transparent"
+         header.style.backgroundColor="rgb(31,30,30)"
          }
      
      
